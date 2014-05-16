ac3d814d3f0c2ae9777ef1a69d1919ac81548720
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2014-05-16 11:16:57 +0300

    Revert "Initial commits on lab module"
    This reverts commit f89e9872cf3912df9c62907cb18584aa8b17f2c0.

---
+++ api/src/main/java/org/openmrs/module/keaddonexample/metadata/ExampleMetadata.java
@@ -0,0 +1,45 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.keaddonexample.metadata;
+
+import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.springframework.stereotype.Component;
+
+import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.*;
+
+/**
+ * Example metadata bundle
+ */
+@Component
+public class ExampleMetadata extends AbstractMetadataBundle {
+
+	public static class _EncounterType {
+		public static final String EXAMPLE = "d69dedbd-3933-4e44-8292-bea939ce980a";
+	}
+
+	public static class _Form {
+		public static final String EXAMPLE = "b694b1bc-2086-47dd-a4ad-ba48f9471e4b";
+	}
+
+	/**
+	 * @see org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle#install()
+	 */
+	@Override
+	public void install() {
+		install(encounterType("Example encounter", "Just an example", _EncounterType.EXAMPLE));
+
+		install(form("Example form", null, _EncounterType.EXAMPLE, "1", _Form.EXAMPLE));
+	}
+}
---
+++ omod/src/main/webapp/pages/example/home.gsp
@@ -0,0 +1,7 @@
+<%
+	ui.decorateWith("kenyaemr", "standardPage", [ patient: patient ])
+%>
+
+<div class="ke-page-content">
+	${ ui.decorate("kenyaui", "panel", [ heading: "Welcome" ], "This is an example page added by this module") }
+</div>
